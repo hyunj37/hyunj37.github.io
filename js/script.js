aab10b08004292199c2888e20b863a964432af69
hyunj <hyunjeong670@gmail.com>
2019-09-13 00:48:39 +0900

    2019-09-13 contact, responsible setting

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//toggle
+var show = false;
+var scrollHeight = $(document).height();
+$('.contact h3').click(function(){
+   if(show===false){
+        $('.email').slideDown();
+        $("body,html").animate({scrollTop:scrollHeight},800);
+        show = true;
+   }else{
+        $('.email').slideUp();
+        show = false;
+   }
+});
